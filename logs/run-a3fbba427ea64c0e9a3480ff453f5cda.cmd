4aab4c34916d04fcc0ca1d4968ed34c28bcb3211
ckbenimaru <47964555+ckbenimaru@users.noreply.github.com>
2026-07-10 09:07:31 +0800

    Delete logs directory

--- logs/run-a3fbba427ea64c0e9a3480ff453f5cda.cmd
+++
@@ -1,5 +0,0 @@
-﻿@echo off
-chcp 65001 >nul
-cd /d "D:\下载\SmartVideoDownloader"
-"D:\下载\SmartVideoDownloader\yt-dlp.exe" "--proxy" "socks5://127.0.0.1:7898" "--cookies" "D:\下载\SmartVideoDownloader\cookies.txt" "-f" "bv*+ba/b" "--merge-output-format" "mp4" "--write-thumbnail" "--embed-thumbnail" "--embed-metadata" "--no-mtime" "--windows-filenames" "--restrict-filenames" "--retries" "10" "--fragment-retries" "10" "--concurrent-fragments" "3" "-P" "D:\下载\SmartVideoDownloader\downloads" "-o" "%(title).80s-%(id)s.%(ext)s" "https://hk.pornhub.com/view_video.php?viewkey=6a40f609a2abf" > "D:\下载\SmartVideoDownloader\logs\20260709-095751-e3fdbf03.log" 2>&1
-
